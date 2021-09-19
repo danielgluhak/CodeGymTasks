9c34370f520935be11693d4ee48084f72415d6be
MojProfil <gluhakdaniel@gmail.com>
2021-09-20 00:08:23 +0200

    Update MinesweeperGame.java

--- 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
@@ -1,7 +1,11 @@
 package com.codegym.games.minesweeper;
 
+import com.codegym.engine.cell.Color;
 import com.codegym.engine.cell.Game;
 import com.codegym.engine.cell.*;
+import org.w3c.dom.Node;
+
+import java.awt.*;
 
 public class MinesweeperGame extends Game {
 
@@ -13,6 +17,8 @@ public class MinesweeperGame extends Game {
     public void setScreenSize(int x, int y) {
         int size = x*y;
     }
+
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
 
 }
