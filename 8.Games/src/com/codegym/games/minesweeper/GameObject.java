cd57b80b3a75546274652bc71001db058980a3dd
MojProfil <gluhakdaniel@gmail.com>
2021-09-20 00:08:18 +0200

    Create GameObject.java

---
+++ 8.Games/src/com/codegym/games/minesweeper/GameObject.java
@@ -0,0 +1,26 @@
+package com.codegym.games.minesweeper;
+
+import com.codegym.engine.cell.Color;
+import com.codegym.engine.cell.Game;
+import org.w3c.dom.Node;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.security.cert.PolicyNode;
+
+public class GameObject {
+
+
+    public int x;
+    public int y;
+
+
+    public GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+
